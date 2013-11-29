e01ed15a573428f8d30553a610ee0dc226e4f63a
Eric Fulmer <EFulmer@gmail.com>
2013-11-29 01:00:18 -0500

    extra cred for challenge 139

--- challenge_139.py
+++ challenge_139.py
@@ -1,7 +1,34 @@
 # Daily Programmer Challenge #139: 
 # Pangrams (Easy)
 
-def is_pangram(string):
-    alphabet = { chr(i) for i in range(96, 123) }
-    lc_str = string.lower()
-    return all(alpha in lc_str for alpha in alphabet)
+from collections import Counter
+from string import ascii_lowercase
+
+
+def is_pangram(a_string):
+    lc_str = a_string.lower()
+    in_str = [ char in lc_str for char in ascii_lowercase ]
+    return all(in_str)
+
+
+def pangram_ec(a_string):
+    lc_str = a_string.lower()
+    char_count = Counter(lc_str)
+    count_str = [ '{char}: {num}'.format(char=k, num=char_count[k]) 
+                  for k in sorted(list(set((char_count.elements()))))
+                  if k in ascii_lowercase ]
+    in_str = [ char in lc_str for char in ascii_lowercase ]
+    # not a fan of this + ' ' bit:
+    result_str = str(all(in_str)) + ' ' + ' '.join(count_str)
+    return result_str
+
+
+def main():
+    n = int(input(''))
+    strings = [ input('') for i in range(n) ]
+    for s in strings:
+        print(pangram_ec(s))
+
+
+if __name__ == '__main__':
+    main()
