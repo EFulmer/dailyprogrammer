dcfdd06747d2741370b371e6c6e881ba447a9785
Eric Fulmer <EFulmer@gmail.com>
2013-11-30 17:41:24 -0500

    cleaned up challenge 134

--- challenge_134.py
+++ challenge_134.py
@@ -3,7 +3,6 @@
 
 def n_divisible(n, m):
     biggest_num = 10 ** m - 1
-    # print('n = {n}, m = {biggest_num}'.format(n=n, biggest_num=biggest_num))
     for i in range(biggest_num, n, -1):
         if i % n == 0:
             return i
