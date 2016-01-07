d14e4790bf194d64864aaf16a14bb6437641bd26
Eric Fulmer <EFulmer@gmail.com>
2016-01-07 00:29:19 -0500

    more on the challenge 248

--- Challenge248.hs
+++ Challenge248.hs
@@ -1,10 +1,23 @@
+import qualified Data.Map as Map
 import System.Environment
 import System.Exit
 import System.IO
 import Text.Read
--- words 
 
-data ChallengeData = ChallengeData { inH :: Handle, outH :: Handle, numRows :: Int, numCols :: Int }
+type Point   = (Int, Int)
+type Color   = (Int, Int, Int)
+type Picture = Map.Map Point Color
+
+data ChallengeData = ChallengeData { inH  :: Handle
+                                   , outH :: Handle
+                                   , numRows :: Int
+                                   , numCols :: Int
+                                   , picture :: Map.Map Point Color }
+
+-- readSafeLine :: String -> [Int]
+-- readSafeLine line = (map fst) (map reads (words line))
+readSafeLine :: String -> [Int]
+readSafeLine str = (map fst) (concatMap reads (words str))
 
 main :: IO ()
 main = do
@@ -16,40 +29,46 @@ main = do
         inF <- openFile inFName ReadMode
         outF <- openFile "out" WriteMode -- TODO choose name based on inFName.
         hPutStrLn outF "P3"
-        -- putStrLn inFName
         inStr <- hGetLine inF -- rename 1stline or something
-        -- reads
-        let line1 = map fst $ concat (map reads (words inStr) :: [[(Int, String)]])
-        -- print line1
+        let line1 = readSafeLine inStr
         let (cols, rows) = (line1 !! 0, line1 !! 1)
-        -- print (cols, rows)
         hPutStrLn outF inStr
-        let cData = ChallengeData { inH = inF, outH = outF, numCols = cols, 
-            numRows = rows }
-        mainLoop cData
+        contents <- hGetContents inF
+        -- let finalPicture = mainLoop (lines contents) Map.empty
+        -- let picString = printPicture finalPicture cols rows
+        -- hPutStr outF picString
+        hPutStr outF contents
         hClose inF
         hClose outF
 
-mainLoop :: ChallengeData -> IO ()
-mainLoop cData = do
-    eof <- hIsEOF (inH cData)
-    case eof of
-        True  -> return ()
-        False -> do
-            line <- hGetLine (inH cData)
-            let ws = words line
-            let intWords = map fst $ concat (map reads ws :: [[(Int, String)]])
-            case ws of
-                ("point":ws) -> printPoint cData intWords
-                ("line":ws)  -> printLine cData intWords
-                ("rect":ws)  -> printRect cData intWords
-                _            -> do die "every line should start with 'point', 'line', or 'rect'"
+mainLoop :: [String] -> Picture -> Picture
+mainLoop [] picture     = picture
+mainLoop (x:xs) picture = case words x of
+                              ("point":xs') -> mainLoop xs' $ drawPoint xs' picture
+                              ("line":xs')  -> mainLoop xs' $ drawLine xs' picture
+                              ("rect":xs')  -> mainLoop xs' $ drawRect xs' picture
 
-printPoint :: ChallengeData -> [Int] -> IO ()
-printPoint cData xs = undefined
 
-printLine :: ChallengeData -> [Int] -> IO ()
-printLine cData xs = undefined
+drawPoint :: [String] -> Picture -> Picture
+drawPoint xs pic = let xs'        = readSafeLine (concat xs)
+                       (r, g, b)  = (xs' !! 0, xs' !! 1, xs' !! 2)
+                       (x, y)     = (xs' !! 4, xs' !! 3)
+                   in Map.insert (x, y) (r, g, b) pic
 
-printRect :: ChallengeData -> [Int] -> IO ()
-printRect cData xs = undefined
+drawLine :: [String] -> Picture -> Picture
+drawLine xs pic =  undefined
+
+drawRect :: [String] -> Picture -> Picture
+drawRect xs pic = let xs'       = readSafeLine (concat xs)
+                      (r, g, b) = (xs' !! 0, xs' !! 1, xs' !! 2)
+                      (x, y)    = (xs' !! 4, xs' !! 3)
+                      (h, l)    = (xs' !! 5, xs' !! 6)
+                      newMapAL  = [ ((a, b), (r, g, b)) | 
+                                    a <- [x..x+h], b <- [y..y+l] ]
+                  in Map.union (Map.fromList newMapAL) pic
+
+-- printPicture :: Picture -> Int -> Int -> String
+printPicture pic cols rows = let colors = [ [ show $ Map.findWithDefault (0, 0, 0) (r, c) pic | 
+                                            r <- [0..rows-1] ] | c <- [0..cols-1] ]
+              
+                             in unlines (map unwords colors)
