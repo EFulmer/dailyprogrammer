ecca576fe6a0fa219218e9335b1823fb0e6faac6
Eric Fulmer <EFulmer@gmail.com>
2016-03-09 00:57:07 -0500

    challenge 257 in haskell

---
+++ Challenge257.hs
@@ -0,0 +1,60 @@
+module Challenge257 where
+import qualified Data.IntMap as IntMap
+import Data.Maybe (catMaybes)
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
+import Text.Regex
+
+type Lifespan = (Int, Maybe Int)
+
+csvSplitRE :: Regex
+csvSplitRE = mkRegex ",[ t]+" 
+
+splitCSVLine :: String -> [String]
+splitCSVLine = splitRegex csvSplitRE
+
+getLifespan :: [String] -> Maybe Lifespan -- Maybe accounts for headers
+getLifespan (pres:bday:bloc:dday:dloc:[]) = do
+  bday' <- readMaybe $ last4 bday
+  if (not . null) dday
+  then Just (bday', readMaybe $ last4 dday)
+  else Just (bday', Nothing)
+  where 
+    last4 ss = drop (length ss - 4) ss
+getLifespan _ = Nothing
+
+bothAlive :: Lifespan -> Lifespan -> Bool
+bothAlive (b1, d1) (b2, d2) = case (d1, d2) of
+  (Just d1', Just d2') -> max b1 b2 <= min d1' d2'
+  (Just d1', Nothing)  -> max b1 b2 <= d1'
+  (Nothing, Just d2')  -> bothAlive (b2, d2) (b1, d1)
+  (Nothing, Nothing)   -> True
+
+lifespanToRange :: Lifespan -> [Int]
+lifespanToRange (a1, Nothing) = [a1..2016]
+lifespanToRange (a1, Just a2) = [a1..a2]
+
+buildFreqTable :: [Int] -> IntMap.IntMap Int
+buildFreqTable xs = acc xs IntMap.empty
+  where acc (x:xs) m = if IntMap.member x m
+                       then acc xs $ IntMap.adjust succ x m
+                       else acc xs $ IntMap.insert x 1 m
+        acc []     m = m
+
+maxVal :: IntMap.IntMap Int -> (Int, Int) -- (numAlive, year)
+maxVal m = maximum $ map swap $ IntMap.toList m
+  where swap (a, b) = (b, a)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  if length args /= 1
+  then putStrLn "usage: Challenge257 [csv-name]"
+  else do
+    pd <- readFile $ args !! 0
+    let pd' = fmap splitCSVLine (lines pd)
+    let lifespans = fmap getLifespan pd'
+    let lifeYears = concat $ fmap lifespanToRange (catMaybes lifespans)
+    let freqTable = buildFreqTable lifeYears
+    let (alive, year) = maxVal freqTable
+    putStrLn $ show alive ++ " presidents were alive in " ++ show year ++ "."
