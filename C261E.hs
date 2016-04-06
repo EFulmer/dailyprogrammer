9e181d14feebd783d5f8a0d5ca37f556d1a0cb05
Eric Fulmer <EFulmer@gmail.com>
2016-04-05 21:21:18 -0400

    bonus 2 of challenge 261

--- C261E.hs
+++ C261E.hs
@@ -1,8 +1,9 @@
 module C261E where
 
-import Data.List  (transpose)
-import Data.Maybe (catMaybes)
-import Text.Read  (readMaybe)
+import           Data.List         (permutations, transpose)
+import           Data.Maybe        (catMaybes)
+import qualified Data.Set   as Set
+import           Text.Read         (readMaybe)
 
 toListOfLists :: [Int] -> Int -> [[Int]]
 toListOfLists xs n = case splitAt n xs of
@@ -13,7 +14,7 @@ fwdDiag :: [[a]] -> [a]
 fwdDiag xs = fmap (\(i, xs) -> xs !! i) $ zip [0..] xs
 
 revDiag :: [[a]] -> [a]
-revDiag xs = fwdDiag $ reverse xs
+revDiag = fwdDiag . reverse
 
 isMagicSquare :: [Int] -> Bool
 isMagicSquare xs = let
@@ -26,9 +27,34 @@ isMagicSquare xs = let
   in
     all check [xsMat, xsMat', xsMatDiags]
 
+isMagicSquare' :: [[Int]] -> Bool
+isMagicSquare' sq = let
+  n = length sq
+  ans = (n * (n ^ 2 + 1)) `div` 2
+  check = all (\x -> sum x == 15)
+  diags = [fwdDiag sq, revDiag sq]
+  in
+    all check [sq, diags]
+
+msChall :: [Int] -> Int -> Bool
+msChall xs n = let
+  missing = Set.toList $ (Set.fromList [1..n^2]) Set.\\ (Set.fromList xs)
+  perms   = permutations missing
+  in
+    or $ fmap isMagicSquare [ xs ++ perm | perm <- perms ]
+
+
 main :: IO ()
 main = do
-  putStr "Enter numbers: "
-  square <- getLine
-  let stuff = catMaybes $ fmap readMaybe (words square) :: [Int]
-  print $ isMagicSquare stuff
+  putStr "Enter dimension of magic square to verify: "
+  n <- getLine
+  putStrLn $ "Enter " ++ n ++ " lines, one for each row of the magic square, numbers separated by spaces."
+  let n' = readMaybe n
+  case n' of
+    Nothing -> putStrLn "Please enter a valid number."
+    Just n'' -> do
+    let lns = replicate n'' getLine
+    square <- sequence lns
+    let sq = fmap catMaybes $ (fmap . fmap) readMaybe (fmap words square) :: [[Int]]
+    print $ isMagicSquare' sq
+
