2304e277c51bd4a65e68cb7a0bee41be1dbbf25b
Eric Fulmer <EFulmer@gmail.com>
2016-04-06 21:26:42 -0400

    fixed bug in C261E

--- C261E.hs
+++ C261E.hs
@@ -5,10 +5,8 @@ import           Data.Maybe        (catMaybes)
 import qualified Data.Set   as Set
 import           Text.Read         (readMaybe)
 
-toListOfLists :: [Int] -> Int -> [[Int]]
-toListOfLists xs n = case splitAt n xs of
-  (xs1, [])  -> [xs1]
-  (xs1, xs2) -> xs1:(toListOfLists xs2 n)
+cTestTrue = [[8, 1, 6], [3, 5, 7]]
+cTestFalse = [[3, 5, 7], [8, 1, 6]]
 
 fwdDiag :: [[a]] -> [a]
 fwdDiag xs = fmap (\(i, xs) -> xs !! i) $ zip [0..] xs
@@ -16,33 +14,26 @@ fwdDiag xs = fmap (\(i, xs) -> xs !! i) $ zip [0..] xs
 revDiag :: [[a]] -> [a]
 revDiag = fwdDiag . reverse
 
-isMagicSquare :: [Int] -> Bool
-isMagicSquare xs = let
-  n            = (truncate . sqrt . fromIntegral . length) xs
-  ans          = (n * (n ^ 2 + 1)) `div` 2
-  check        = all (\x -> sum x == 15)
-  xsMat        = toListOfLists xs n
-  xsMat'       = transpose xsMat
-  xsMatDiags   = [fwdDiag xsMat, revDiag xsMat]
-  in
-    all check [xsMat, xsMat', xsMatDiags]
-
-isMagicSquare' :: [[Int]] -> Bool
-isMagicSquare' sq = let
-  n = length sq
-  ans = (n * (n ^ 2 + 1)) `div` 2
-  check = all (\x -> sum x == 15)
+isMagicSquare :: [[Int]] -> Bool
+isMagicSquare sq = let
+  n     = length sq
+  ans   = (n * (n ^ 2 + 1)) `div` 2
+  check = all (\x -> sum x == ans)
   diags = [fwdDiag sq, revDiag sq]
   in
     all check [sq, diags]
 
-msChall :: [Int] -> Int -> Bool
+msChall :: [[Int]] -> Int -> Bool
 msChall xs n = let
-  missing = Set.toList $ (Set.fromList [1..n^2]) Set.\\ (Set.fromList xs)
+  missing = Set.toList $ (Set.fromList [1..n^2]) Set.\\ (Set.fromList (concat xs))
   perms   = permutations missing
   in
-    or $ fmap isMagicSquare [ xs ++ perm | perm <- perms ]
+    or $ fmap isMagicSquare [ xs ++ [perm] | perm <- perms ]
 
+msChall' :: [[Int]] -> Bool
+msChall' xs = let
+  undefined = undefined
+  in undefined
 
 main :: IO ()
 main = do
@@ -56,5 +47,5 @@ main = do
     let lns = replicate n'' getLine
     square <- sequence lns
     let sq = fmap catMaybes $ (fmap . fmap) readMaybe (fmap words square) :: [[Int]]
-    print $ isMagicSquare' sq
+    print $ isMagicSquare sq
 
